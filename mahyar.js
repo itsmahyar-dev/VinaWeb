a3eb38cc3c0b7cdd2e90f67a923e04ff36277c1b
mahyarghofrany <mahyarghofrany9@gmail.com>
2026-06-24 10:59:47 +0330

    finaly project

--- mahyar.js
+++ mahyar.js
@@ -1,11 +1,41 @@
-const acc = document.querySelector(".accordion");
-const panel = document.querySelector(".panel");
+document.addEventListener('DOMContentLoaded',()=>{
+  const counters = document.querySelectorAll(".counter");
 
-acc.addEventListener("click", function () {
-  acc.classList.toggle("active");
-  if (panel.style.maxHeight) {
-    panel.style.maxHeight = null;
-  } else {
-    panel.style.maxHeight = panel.scrollHeight + "px";
+  let started = false;
+
+  function startCounters() {
+    if (started) return;
+
+    const statsSection = document.querySelector(".stats");
+    const sectionTop = statsSection.offsetTop;
+    const scrollPosition = window.scrollY + window.innerHeight;
+
+    if (scrollPosition > sectionTop + 100) {
+      started = true;
+
+      counters.forEach((counter) => {
+        const target = +counter.dataset.target;
+        const speed = target / 100;
+
+        function updateCounter() {
+          const current = +counter.innerText;
+
+          if (current < target) {
+            counter.innerText = Math.ceil(current + speed);
+
+            requestAnimationFrame(updateCounter);
+          } else {
+            counter.innerText = target + "+";
+          }
+        }
+
+        updateCounter();
+      });
+    }
   }
-});
+
+  window.addEventListener("scroll", startCounters);
+
+  startCounters();
+
+})
